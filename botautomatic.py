d28a1976bd2d3935d4d91babe4622cda42e5688f
Virus-33 <51784979+Virus-33@users.noreply.github.com>
2023-08-12 11:10:44 +0000

    Added not an error logging

--- botautomatic.py
+++ botautomatic.py
@@ -7,8 +7,8 @@ from modules import module_logger
 import pickle
 import os
 
-keep_alive()
-
+#keep_alive()
+skip = 0
 token = os.environ['token']
 
 # время для постинга
@@ -71,7 +71,10 @@ def get_last_post(_tg: int):
                 return None
             else:
                 if wt == 1:
-                    last_post = next((x for x in posts if x['from_id'] == bot_id), None)
+                    for x in posts:
+                        if x['from_id'] == bot_id:
+                            last_post = x
+                            break
                 elif wt == 2 or wt == 3:
                     for x in posts:
                         if 'signer_id' in x:
@@ -85,40 +88,42 @@ def get_last_post(_tg: int):
                 return last_post
         return last_post
     except Exception as e:
-        module_logger.Log(f'Something happened during postwatch in {target_group}\n' + str(e))
+        module_logger.eLog(f'Something happened during postwatch in {target_group}\n' + str(e))
         return -1
-    return last_post
 
 
 def check_suggests(_tg: int, time: int):
     # noinspection PyUnusedLocal
     suggested_posts = vk.wall.get(owner_id=-target_group, filter='suggests')
+    last_pst = get_last_post(_tg)
     if len(suggested_posts['items']) < 1:
         if _tg in time_dict:
             if datetime.now() - time_dict[target_group] >= timedelta(seconds=time):
-                module_logger.Log(
-                    f'Post in group {target_group} timed for too many days or more. Strange.'
+                module_logger.eLog(
+                    f'Post in group {target_group} timed for too many days. Strange.'
                     f' Posting again to remind of myself')
                 return 1
             else:
                 return 0
-        elif last_pst := get_last_post(_tg) is not None:
+        elif last_pst is not None:
             if last_pst != -1:
                 if datetime.now() - datetime.fromtimestamp(last_pst['date']) >= timedelta(seconds=time):
                     return 1
                 else:
                     return 0
             else:
-                return 0
+                return -1
         else:
             if _tg in time_dict:
                 if datetime.now() - time_dict[target_group] >= timedelta(seconds=time):
                     return 1
                 else:
                     return 0
+            else:
+                return 1
     suggest_time = suggested_posts['items'][0]['date']
     if datetime.now() - datetime.fromtimestamp(suggest_time) >= timedelta(seconds=time):
-        module_logger.Log(
+        module_logger.eLog(
             f'Post in group {target_group} delayed for 3 days or more. Dead one?'
             f' Posting again to remind of myself')
         return 1
@@ -132,39 +137,61 @@ def choose_time(_timer):
         return _timer
 
 
-
 while True:
     try:
         with open('files/groups.txt', 'r', encoding='utf-8') as file:
             while string := file.readline():
+                if skip == 1:
+                    skip = 0
+                    continue
                 target_group, text, image, timer = parse(string)
                 text, time_dict = prepare(text)
+                module_logger.Log(f"Now working with group {target_group}")
 
                 group = vk.groups.getById(group_id=target_group, fields='wall')
                 wall_type = group[0]['wall']
+                module_logger.Log("Got the wall type")
 
                 if wall_type == 2 or wall_type == 3:
                     should_post = check_suggests(target_group, choose_time(timer))
+                    module_logger.Log("Should I post here?")
                     if should_post == 1:
+                        module_logger.Log("Definitly yes")
                         vk.account.setOnline()
                         post(target_group, text, image)
                         time_dict[target_group] = datetime.now()
+                        module_logger.Log("Posted and saved time")
                         with open('files/dumping.pkl', 'wb+') as p:
                             pickle.dump(time_dict, p)
+                    if should_post == -1:
+                        module_logger.Log("Absolutly no")
+                        vk.account.setOffline()
+                        sleep(randint(30, 468))
+                        continue
                 elif wall_type == 1:
                     temp_time = choose_time(timer)
+                    module_logger.Log("Choosed time to post")
                     last_bot_post = get_last_post(target_group)
                     if last_bot_post is None:
+                        module_logger.Log("Can't find my post! Posting right now...")
                         vk.account.setOnline()
                         post(target_group, text, image)
                     elif last_bot_post != -1:
+                        module_logger.Log("Found a post, but I need another one")
                         # noinspection PyUnresolvedReferences
                         post_time = last_bot_post['date']
                         if datetime.fromtimestamp(post_time) <= datetime.now() - timedelta(seconds=temp_time):
                             vk.account.setOnline()
                             post(target_group, text, image)
-        vk.account.setOffline()
-        sleep(randint(30, 468))
+                    elif last_bot_post == -1:
+                        module_logger.Log("My job here is done")
+                        vk.account.setOffline()
+                        sleep(randint(30, 468))
+                        continue
+                vk.account.setOffline()
+                module_logger.Log("Sleep for next iteration")
+                #sleep(randint(30, 468))
     except Exception as e:
-        module_logger.Log(str(target_group) + ' ' + str(e))
+        module_logger.eLog(str(target_group) + ' ' + str(e))
         sleep(60)
+        skip = 1
--- modules/module_logger.py
+++ modules/module_logger.py
@@ -1,8 +1,14 @@
 import datetime
 
-log = open("error_log.txt", "a+", encoding='utf-8')
+elog = open("error_log.txt", "a+", encoding='utf-8')
+log = open("log.txt", "a+", encoding='utf-8')
 
 
-def Log(error):
-    log.write("[" + str(datetime.datetime.now().date()) + ' ' + str(datetime.datetime.now().timetz()) + "] " + str(error) + "\n")
+def eLog(error):
+    elog.write("[" + str(datetime.datetime.now().date()) + ' ' + str(datetime.datetime.now().timetz()) + "] " + str(error) + "\n")
+    elog.flush()
+
+
+def Log(action):
+    log.write("[" + str(datetime.datetime.now().date()) + ' ' + str(datetime.datetime.now().timetz()) + "] " + str(action) + "\n")
     log.flush()
